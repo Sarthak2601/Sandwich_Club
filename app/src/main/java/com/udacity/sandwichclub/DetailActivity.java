833956cb38ab767ffa4d94fa10da0fe8d99f7e19
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-28 19:36:26 +0530

    formatting

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -71,7 +71,7 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
-        if(getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
@@ -87,42 +87,39 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-        if(!sandwich.getPlaceOfOrigin().isEmpty()){
+        if (!sandwich.getPlaceOfOrigin().isEmpty()) {
             placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
-        }
-        else{
+        } else {
             placeOfOriginHeading.setVisibility(View.GONE);
             placeOfOrigin.setVisibility(View.GONE);
         }
 
         description.setText(sandwich.getDescription());
         List<String> alsoKnown = sandwich.getAlsoKnownAs();
-        populateLists(alsoKnown,knownAsLL,alsoKnownAsHeading);
+        populateLists(alsoKnown, knownAsLL, alsoKnownAsHeading);
         List<String> ingredientsList = sandwich.getIngredients();
-        populateLists(ingredientsList,ingredientsLL,ingredientsHeading);
+        populateLists(ingredientsList, ingredientsLL, ingredientsHeading);
 
     }
 
-    private void newTextviews(LinearLayout linearLayout, String string){
+    private void newTextviews(LinearLayout linearLayout, String string) {
         LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         TextView textView = new TextView(this);
         textView.setLayoutParams(layoutParams);
-        textView.setText("\u2022 "+string);
+        textView.setText("\u2022 " + string);
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
         textView.setTextColor(getResources().getColor(android.R.color.black));
         linearLayout.addView(textView);
 
-
     }
 
-    private void populateLists(List<String> list,LinearLayout linearLayout, TextView textView){
-        if(!list.isEmpty()) {
+    private void populateLists(List<String> list, LinearLayout linearLayout, TextView textView) {
+        if (!list.isEmpty()) {
             for (int i = 0; i < list.size(); i++) {
-                String value =  list.get(i);
-                newTextviews(linearLayout,value);
+                String value = list.get(i);
+                newTextviews(linearLayout, value);
             }
-        }
-        else{
+        } else {
             textView.setVisibility(View.GONE);
             linearLayout.setVisibility(View.GONE);
         }
